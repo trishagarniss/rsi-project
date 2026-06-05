b3d2c0d739f2ffa4417e5b557b7ff9d548ca0db3
trishagarniss <trishagarniss@gmail.com>
2026-06-05 13:06:04 +0700

    update

--- backend/src/backend/dto/tenant_dto.py
+++ backend/src/backend/dto/tenant_dto.py
@@ -7,9 +7,8 @@ from src.backend.models.enums import TenantStatus
 # 1. Skema untuk Validasi Input (Request dari Frontend)
 class TenantCreateDTO(BaseModel):
     name: str = Field(..., min_length=3, max_length=150, description="Nama sekolah minimal 3 karakter")
-    address: Optional[str] = Field(None, max_length=255) # Disesuaikan dengan model.py
+    address: Optional[str] = Field(None, max_length=255) 
     contact_email: Optional[EmailStr] = Field(None, description="Email valid sekolah")
-    registration_code: str
     status: TenantStatus = TenantStatus.ACTIVE
 
 # 2. Skema untuk Validasi Output (Response ke Frontend)
@@ -18,6 +17,7 @@ class TenantResponseDTO(BaseModel):
     name: str
     address: Optional[str]
     contact_email: Optional[str]
+    registration_code: str
     status: TenantStatus
     created_at: datetime
     updated_at: Optional[datetime]
--- backend/src/backend/middlewares/auth.py
+++ backend/src/backend/middlewares/auth.py
@@ -1,3 +1,4 @@
+import bcrypt
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 import secrets
@@ -18,11 +19,14 @@ security = HTTPBearer()
 ACCESS_TOKEN_TTL = 900
 REFRESH_TOKEN_TTL = 10800
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
